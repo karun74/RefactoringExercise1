82817cbcd920b9bf5f910aeef6a229aee28192c8
karun74 <sknithi@yahoo.com>
2016-04-06 14:02:22 +0530

    Changed to use static constants for expected results

--- RefactoringExercise1/src/com/sapient/utility/OrderUtilityUnitTest.java
+++ RefactoringExercise1/src/com/sapient/utility/OrderUtilityUnitTest.java
@@ -12,32 +12,26 @@ public class OrderUtilityUnitTest {
 
 	private OrderUtilityInterface orderUtil = new OrderUtility();
 	
+	private static final String expectedResultStr = "two thousand five hundred twenty-three";
+	private static final Integer expectedResultInt = 2523;
+	
 	@Test
 	public void testConvertToText() {
-		Order order = new Order();
-		double orderAmount = 2522.70;
-		String expectedResultStr = "2523";
-		order.setOrderAmount(orderAmount);
+		Order order = new Order(2522.70);
 		String resultStr = orderUtil.convertToText((int)Math.round(order.getOrderAmount()));
 		assertEquals(expectedResultStr, resultStr);
 	}
 
 	@Test
 	public void testRoundToNearestNumber(){
-		Order order = new Order();
-		double orderAmount = 2522.70;
-		Integer expectedResultInt = 2523;
-		order.setOrderAmount(orderAmount);
+		Order order = new Order(2522.70);
 		Integer roundedNumberResult = orderUtil.roundToNearestNumber(order.getOrderAmount());
 		assertEquals(expectedResultInt, roundedNumberResult);
 	}
 	
 	@Test
-	public void testRoundAndConvert(){
-		Order order = new Order();
-		double orderAmount = 2522.70;
-		order.setOrderAmount(orderAmount);
-		String expectedResultStr = "2523";
+	public void testRoundAndConvertText(){
+		Order order = new Order(2522.70);
 		String orderAmountStrResult = orderUtil.roundAndConvert(order.getOrderAmount());
 		assertEquals(expectedResultStr, orderAmountStrResult);
 		
